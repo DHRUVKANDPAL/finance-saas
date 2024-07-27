6973c412b0bff49a8b532964e2a33ab88084fde9
DHRUVKANDPAL <dhruvkandpal172@gmail.com>
2024-07-27 17:25:33 +0530

    commit

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,14 @@
+import accounts from "./accounts";
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+export const runtime = "edge";
+
+const app = new Hono().basePath("/api");
+
+const routes = app.route("/accounts", accounts);
+
+export const GET = handle(app);
+export const POST = handle(app);
+
+export type AppType = typeof routes;
